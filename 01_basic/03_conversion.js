a71feb4cb3564bb006ba2bb8a49195d022b83662
Raj-Coderhub <rajgautamindustries@gmail.com>
2025-02-08 20:02:20 +0000

    conversion

---
+++ 01_basic/03_conversion.js
@@ -0,0 +1,18 @@
+let score="33"
+
+console.log(typeof score);
+
+let value =Number(score)
+console.log(typeof value);
+console.log(value);
+
+
+let islogg=1
+let booleanlogg =Boolean(islogg)
+console.log(booleanlogg);
+
+let someNumber =33
+let stringnumber= String(someNumber)
+console.log(typeof stringnumber);
+
+
