fac299b8fce51b7c1837034fae714516c27366a5
Raj-Coderhub <rajgautamindustries@gmail.com>
2025-02-08 19:27:54 +0000

    variable

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,19 @@
+const accountId= 144553
+let accountEmail= "raj@gmail.com"
+var accountpassword="12345"
+accountCity="jaipur"
+let accountSate;
+
+// accountCity=2 // npt allowed
+accountEmail="git@gmail.com"
+accountpassword="225352"
+accountCity="Benguluru"
+
+
+ console.log(accountId);
+ console.table([accountId,accountEmail,accountpassword,accountCity,accountSate]);
+ 
+
+ 
+
+
