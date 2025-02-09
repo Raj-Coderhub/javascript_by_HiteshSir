85944f5673b8247c79dbbddad08d1e0cfdcc21c2
Raj-Coderhub <rajgautamindustries@gmail.com>
2025-02-09 21:09:06 +0000

    comparion

---
+++ 01_basic/04_comparion.js
@@ -0,0 +1,31 @@
+console.log(2>1);
+
+console.log("2">1);
+console.log("2"<1);
+
+console.log(null);
+
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);// comparion and equality work differenty
+
+console.log(undefined);
+
+
+console.log(undefined==0);
+console.log(undefined>0);
+console.log(undefined>=0);
+console.log(undefined<0);
+
+console.log("2"==2);
+console.log("2"===2);
+
+//string,number,boolean,null,undefined, symbol,Bigint
+const id= Symbol('123')
+const anotherid =Symbol('123')
+
+console.log(id===anotherid);
+
+
+
