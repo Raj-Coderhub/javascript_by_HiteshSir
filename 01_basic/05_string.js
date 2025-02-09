9dbc872fbd8599987c35eae3eb43a5765097b7f7
Raj-Coderhub <rajgautamindustries@gmail.com>
2025-02-09 22:26:57 +0000

    string

---
+++ 01_basic/05_string.js
@@ -0,0 +1,19 @@
+const name= "raj"
+const repo= 50
+console.log(`hello my name is ${name} and my repo is${repo}`);
+
+const gamename =String('raj-fefef')
+console.log(gamename[0]);
+console.log(gamename.__proto__);
+console.log(gamename.length);
+console.log(gamename.toUpperCase());
+console.log(gamename.charAt(2));
+console.log(gamename.indexOf('j'));
+
+const newstr ="  hitesh  "
+console.log(newstr.trim());
+
+const url="https://raj.com/200"
+console.log(url.replace('/200',''));
+console.log(url.includes('sunder'));
+console.log(gamename.split('-'));
