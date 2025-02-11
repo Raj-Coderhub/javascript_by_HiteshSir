f9b32cca9fc42a178fabb28b2e03caf530aaaadf
Raj-Coderhub <rajgautamindustries@gmail.com>
2025-02-11 14:41:43 +0000

    date and time

---
+++ 01_basic/07_datatime.js
@@ -0,0 +1,12 @@
+let mydate = new Date('01-14-2024')
+console.log(mydate);
+console.log(mydate.toDateString());
+console.log(mydate.toLocaleString());
+console.log(mydate.toString());
+
+let mytimestamp= Date.now()
+console.log((mytimestamp));
+console.log(mydate.getTime());
+
+
+
