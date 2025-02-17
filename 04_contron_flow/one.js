a8d187da8f980282c8383da0df9102cda7347e5e
Raj-Coderhub <rajgautamindustries@gmail.com>
2025-02-17 10:35:00 +0000

    control

---
+++ 04_contron_flow/one.js
@@ -0,0 +1,10 @@
+// false ,0,,-0,bigInt,"",null,undefined,NaN
+
+//truthy value
+//"0", 'false'," ", {},function(){}
+
+// let val1 = 5??10
+// let val1 = null??10
+// let val1 = undefined??10
+let val1 = undefined??10??20
+console.log(val1);
---
+++ 04_contron_flow/three.js
@@ -0,0 +1,14 @@
+const arr =[1,2,3,4,5]
+
+for (const num of arr) {
+    console.log(num);    
+}
+
+const map = new Map()
+map.set('in','india')
+map.set('aus','austrailia')
+
+for (const [key,value] of map) {
+    console.log(key,'-', value);    
+}
+
