054267588d17f949f130660d8d9d378e48f36dcb
Raj gautam <rajgautamindustries@gmail.com>
2025-03-13 14:55:50 +0000

    proje-4

---
+++ 07_projects/project-04/chaiaurcode.js
@@ -0,0 +1,19 @@
+const randomNumber = parseInt(Math.random()*100+1)
+const submit = document.querySelector('#subt')
+const userinput = document.querySelector('#guessField')
+const guessSlot = document.querySelector('.guesses')
+const remaining = document.querySelector('.lastResult')
+const loworHi = document.querySelector('.loworHi')
+const starOver  = document.querySelector('.resultParas')
+
+const p = document.createElement('p')
+let prevGuess = []
+let numGuess = 1
+let playgame = true
+function validateGuess(guess){
+
+}
+function checkGuess(guess){
+
+}
+funtion displaymessage
